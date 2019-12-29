f4d85bf6db37652866573b8d01c7dabeea53374c
cycyjing <cycycyjing@gmail.com>
2019-12-30 12:24:11 +1300

    demo05 v2

--- angular-demo05/src/app/app-routing.module.ts
+++ angular-demo05/src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { SettingComponent } from './home/setting/setting.component';
+import { CategoriesComponent } from './product/categories/categories.component';
+import { ListsComponent } from './product/lists/lists.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home', component: HomeComponent,
+    children: [
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'setting', component: SettingComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    ]
+  },
+  {
+    path: 'product', component: ProductComponent,
+    children: [
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: '**', redirectTo: 'categories' },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-demo05/src/app/app.module.ts
+++ angular-demo05/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { SettingComponent } from './home/setting/setting.component';
+import { CategoriesComponent } from './product/categories/categories.component';
+import { ListsComponent } from './product/lists/lists.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductComponent
+    ProductComponent,
+    WelcomeComponent,
+    SettingComponent,
+    CategoriesComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
